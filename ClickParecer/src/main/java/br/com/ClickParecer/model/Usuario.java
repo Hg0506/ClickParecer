9e608e2c6a3d885aef708df3749225d8d38eb0f4
Henrique_G. <henriquegbr2004@gmail.com>
2026-06-09 21:58:24 -0300

    Usuario model

---
+++ ClickParecer/src/main/java/br/com/ClickParecer/model/Usuario.java
@@ -0,0 +1,102 @@
+package br.com.ClickParecer.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "Usuario")
+public class Usuario {
+
+    @Id
+    @Column(name = "idUsuario")
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private int id;
+
+    @Column(nullable = false, name = "NomeUsuario")
+    @NotBlank(message = "O nome do usuário é obrigatório")
+    private String nome;
+
+    @Column(nullable = false, name = "Email")
+    @NotBlank(message = "O email do usuário é obrigatório")
+    private String email;
+
+    @Column(nullable = false, name = "Senha")
+    @NotBlank(message = "A senha do usuário é obrigatória")
+    private String senha;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    // Getter
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    // Setter
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((senha == null) ? 0 : senha.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Usuario other = (Usuario) obj;
+        if (senha == null) {
+            if (other.senha != null)
+                return false;
+        } else if (!senha.equals(other.senha))
+            return false;
+        return true;
+    }
+
+}
